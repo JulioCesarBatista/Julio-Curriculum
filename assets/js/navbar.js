d66ce30bfe53317ced5310b4b9731923c3dbb1d7
Julio Cesar Batista <thecesar165@gmail.com>
2018-12-22 01:23:17 -0200

    Curriculum Atualizado

---
+++ assets/js/navbar.js
@@ -0,0 +1,17 @@
+const menuHamburguer = document.querySelector('.menu-hamburguer');
+const navMobile = document.querySelector('.filtros.mobile');
+
+menuHamburguer.onclick = () => {
+  menuHamburguer.classList.toggle('ativo');
+  navMobile.classList.toggle('ativo');
+}
+
+
+const filtrosMobile = document.querySelector('.filtros.mobile');
+const listaFiltrosMobile = docuemnt.querySelectorAll ('.filtros.mobile a');
+
+for (const filtro of listaFiltrosMobile){
+    filtro.onclick = () => {
+      filtrosMobile.classlist.remove('ativo');
+    }
+}
